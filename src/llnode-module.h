7a894bb1f7945157334e5350ae3a1c84227a7eb7
hyj1991 <hugh.hyj@alibaba-inc.com>
2018-06-14 16:59:57 +0800

    refactor: pagination interface add the 'end' and 'left'(if not end) property   - LLNode::GetThreadInfoById   - LLNode::GetThreadByIds   - LLNode::GetJsObjects   - LLNode::GetJsInstances

--- src/llnode-module.h
+++ src/llnode-module.h
@@ -42,7 +42,7 @@ private:
   static void GetJsObjects(const Nan::FunctionCallbackInfo<Value>& info);
   static void GetJsInstances(const Nan::FunctionCallbackInfo<Value>& info);
   static void InspectJsObjectAtAddress(const Nan::FunctionCallbackInfo<Value>& info);
-  Local<Array> GetThreadInfoById(size_t thread_index, size_t curt, size_t limt);
+  Local<Object> GetThreadInfoById(size_t thread_index, size_t curt, size_t limt);
   Local<Object> InspectJsObject(inspect_t* inspect);
   bool ScanHeap();
 
